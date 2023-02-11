53bd23e26caa034cbce1ba141af2746ef41f0bc0
Vinícius Bohrer <vinicius.bohrer@voalle.com.br>
2023-02-10 22:33:51 -0300

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { MouseEventHandler, ReactNode } from 'react'
+import getQuoteHook from './hooks/getQuoteHook'
+import './style.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const QuoteBox = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <div id="quote-box">{children}</div>
   )
 }
 
-export default App
+const Text = ({ text }: { text: String }) => {
+  return (
+    <p id="text">“{text}”</p>
+  )
+}
+
+const Author = ({ author }: { author: String }) => {
+  return (
+    <p id="author">{author}</p>
+  )
+}
+
+const NewQuote = ({ onClick, disabled }: { onClick: MouseEventHandler<HTMLButtonElement>, disabled: boolean }) => {
+  return (
+    <button id="new-quote" onClick={onClick} disabled={disabled}>Next</button>
+  )
+}
+
+const TweetQuote = ({ onClick, disabled }: { onClick: MouseEventHandler<HTMLButtonElement>, disabled: boolean }) => {
+  return (
+    <button id="tweet-quote" onClick={onClick} disabled={disabled}>Tweet</button>
+  )
+}
+
+export const App = () => {
+  const { quote, author, isLoading, error, rerun }: {
+    quote: String,
+    author: String,
+    isLoading: boolean,
+    error: String,
+    rerun: MouseEventHandler<HTMLButtonElement>
+  } = getQuoteHook()
+
+  const tweetQuoteOnNewTab = (quote: String) => {
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${quote}" - ${author}`)}`
+    window.open(tweetUrl, '_blank')
+  }
+
+  if (error) {
+    return <div id="error">{error}</div>
+  }
+
+  return (
+    <div id="app-container">
+      <QuoteBox>
+        <Text text={quote} />
+        <Author author={author} />
+        <NewQuote disabled={isLoading} onClick={rerun} />
+        <TweetQuote disabled={isLoading} onClick={() => tweetQuoteOnNewTab(quote)} />
+      </QuoteBox>
+    </div>
+  )
+}
---
+++ src/hooks/getQuoteHook.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+
+interface Quote {
+  content: String
+  author: String
+}
+
+const getQuoteHook = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<String>('')
+  const [quote, setQuote] = useState<String>('')
+  const [author, setAuthor] = useState<String>('')
+
+
+  const fetchQuote = async () => {
+    setIsLoading(true)
+
+    try {
+      const response = await fetch('https://api.quotable.io/random')
+      const data: Quote = await response.json()
+
+      setQuote(data.content)
+      setAuthor(data.author)
+      setError('')
+    } catch (err) {
+      setError(`${err}`)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchQuote()
+  }, [])
+
+  const rerun = () => fetchQuote()
+
+  return { quote, author, isLoading, error, rerun }
+}
+
+export default getQuoteHook
